cda6cc56299038e4a58d1661948186822f4c92ea
vijay <vijayabhaskarev@outlook.com>
2021-03-27 19:56:45 +0530

    new

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+
+
+
+
+app.get('/',function(req,res){
+    res.send("app has startes");
+});
+
+
+
+app.listen(3000,function(){
+    console.log("server is running on port 3000");
+});
